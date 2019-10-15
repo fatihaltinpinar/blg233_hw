70682db061fe1677933331ebb06dcd18267f09a7
Fatih ALTINPINAR <fatihaltinpinar@gmail.com>
2019-10-15 09:13:45 +0300

    Add functions

--- hw1.cpp
+++ hw1.cpp
@@ -10,6 +10,7 @@ Date:           13.10.2019
 
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -22,11 +23,88 @@ struct Node{
 struct Stock{
     Node *head;
     void create();
-    void add_stock();
+    void add_stock(int);
     void sell(int);
     void current_stock();
     void clear();
 };
+
+void Stock::create(){
+    head = NULL;
+}
+
+void Stock::add_stock(int size){
+    if(!head){
+        head = new Node;
+        head->size = size;
+        head->quant = 1;
+        head->next = NULL;
+    }else{
+        if(size < head->size){
+            Node *tmp = head;
+            head = new Node;
+            head->size = size;
+            head->quant = 1;
+            head->next = tmp;
+        }else{
+            Node *current = head;
+            Node *next = current->next;
+            while(current){
+                if(size == current->size){
+                    current->quant++;
+                }else if(size < next->size){
+                    current->next = new Node;
+                    current->next->size = size;
+                    current->next->quant = 1;
+                    current->next->next = next;
+                    return;
+                }else{
+                    current = next;
+                    next = current->next;
+                }
+            }
+        } 
+
+    }
+
+}
+
+void Stock::sell(int size){
+    Node *current = head;   
+    while(!(head->next)){
+        if(!(current->next))
+        if(current->next->size == size){
+            
+        }
+
+    }
+
+}
+
+void Stock::current_stock(){
+    Node *current = head;
+    if(!current){
+        cout << "NO_STOCK" << endl;    
+        return;
+    }
+    while(current){
+        cout << current->size << ":" << current->quant << endl;    
+        current = current->next;
+    }
+}
+
+void Stock::clear(){
+    Node *current = head;
+    Node *next = current->next;
+    while(next){
+        delete current;
+        current = next;
+        next = current->next; 
+    }
+    delete current;
+    head = NULL;
+}
+
 
 int main(){
 
