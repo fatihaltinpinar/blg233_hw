f67234cdb0efc81668a8e91a5b4bd76df5561745
Fatih ALTINPINAR <fatihaltinpinar@gmail.com>
2019-11-07 16:13:07 +0300

    Remove constructor since this file cannot be modified

--- HW2/Homework2/task.h
+++ HW2/Homework2/task.h
@@ -5,7 +5,6 @@ PLEASE DO NOT CHANGE THIS FILE
 #define NAME_LENGTH 2
 
 struct Task{
-	//Task() : previous(NULL),next(NULL),counterpart(NULL){} // Default values for previous, next, counterpart 
 	char *name;
 	int day;
 	int time;
