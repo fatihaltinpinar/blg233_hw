cb64b92302856f6c45a41a77cb8f97737e5f7b0e
Fatih Altinpinar <fatihaltinpinar@gmail.com>
2019-12-22 12:40:03 +0300

    Struct definitions

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -8,7 +8,25 @@
 
 using namespace std;
 
-int main(){
+struct MobileHost {
+    int id;
+    MobileHost *next_sibling;
+};
+
+struct BaseStation {
+    int id;
+    BaseStation *child_bs_head;
+    BaseStation *next_sibling;
+    MobileHost *child_mh_head;
+
+    BaseStation* find_base_station(int id);
+    void add_base_station(BaseStation * bs, int parent_id);
+    void add_mobile_host(MobileHost * mh, int parent_id);
+};
+
+
+
+int main() {
 
     cout << "Its time to starth" << endl;
     return 0;
