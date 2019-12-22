78371f7da654a519fd4ca55d3bb0732e0e527cc8
Fatih Altinpinar <fatihaltinpinar@gmail.com>
2019-12-22 11:19:06 +0300

    Init hw3 files

---
+++ HW3/main.cpp
@@ -0,0 +1,15 @@
+/*  @Author
+ *  Student Name:  Fatih Altınpınar
+ *  Student ID:    150180707
+ *  Date:          22/12/2019
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Its time to starth" << endl;
+    return 0;
+}
